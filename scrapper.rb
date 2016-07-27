6f5a281b1b9b97391e4277d6ff6c8d41f5638160
kaiguogit <kaiguo1988@gmail.com>
2016-07-27 21:14:34 +0000

    edited command instruction

--- scrapper.rb
+++ scrapper.rb
@@ -21,22 +21,37 @@ def hit_enter
   end 
 end
 
-if ARGV.length >= 1
-  post = Post.new(ARGV[0])
+def wrong_argument
+  puts "Invalid argument"
+  puts "Usage: ruby scrapper.rb url [switches]"
+  puts "Switches: comments   Display all comments"
+  puts "Example: ruby scrapper.rb https://news.ycombinator.com/item?id=7663775"
+  puts "Example: ruby scrapper.rb https://news.ycombinator.com/item?id=7663775 comments"
+end
+
+def scrape(url)
+  post = Post.new(url)
   puts "Post title: #{post.title}".red.bold.on_white
   puts "Number of comments: #{post.comments.length}".yellow.bold
   puts "Points: #{post.points}".blue.bold
   puts "item_id: #{post.item_id}".magenta.bold
-  case ARGV[1]
+  post
+end
+
+if ARGV.length == 1
+  scrape(ARGV[0])
+elsif ARGV.length == 2
+  case ARGV[1].downcase
   when "comments"
+    post = scrape(ARGV[0])
     post.comments.each_with_index do |comment, index|
       print_comment(comment)
       hit_enter if index % 5 == 4
     end
   else
+    wrong_argument
   end
 else
-  puts "Invalid number of arguments"
-  puts "Usage: ruby scrapper.rb https://news.ycombinator.com/item?id=7663775"
+  wrong_argument
 end
 
